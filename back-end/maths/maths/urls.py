b01f6389ede499d1664c503659ca31cff72faf51
Cosmosquark <cosmosquark@sector-alpha.net>
2019-01-16 23:55:05 +0000

    adding backend user authentication

--- back-end/maths/maths/urls.py
+++ back-end/maths/maths/urls.py
@@ -19,13 +19,10 @@ from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, \
                                             verify_jwt_token
 
-from maths.views import custom404
-
-handler404 = custom404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
     path('token-refresh/', refresh_jwt_token),
-    path('token-verify/', verify_jwt_token),  
+    path('token-verify/', verify_jwt_token),
 ]
