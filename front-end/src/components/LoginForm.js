5fcefaadfff0b1bbbff0a5e0fa32dc77a8695341
Cosmosquark <cosmosquark@sector-alpha.net>
2019-01-17 23:18:31 +0000

    Updated the Navbar to be nicer, updated the login form

--- front-end/src/components/LoginForm.js
+++ front-end/src/components/LoginForm.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    display: 'block', // Fix IE11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE11 issue.
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+});
+
 
 class LoginForm extends React.Component {
-  state = {
-    username: '',
-    password: ''
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
 
   handle_change = e => {
     const name = e.target.name;
@@ -18,8 +64,56 @@ class LoginForm extends React.Component {
   };
 
   render() {
+    const {component: Component, ...classes} = this.props.classes;
     return (
-      <form onSubmit={e => this.props.handle_login(e, this.state)}>
+      <React.Fragment>
+        <CssBaseline />
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockIcon />
+            </Avatar>
+
+            <Typography variant="headline">Sign In To The Quiz</Typography>
+            <form className={classes.form} onSubmit={e => this.props.handle_login(e, this.state)}>
+
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="username">Username</InputLabel>
+                <Input
+                id="username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                value={this.state.username}
+                onChange={this.handle_change}/>
+              </FormControl>
+
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={this.state.password}
+                  onChange={this.handle_change}/>
+              </FormControl>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="raised"
+                color="primary"
+                className={classes.submit}>
+                Sign in
+              </Button>
+
+            </form>
+          </Paper>
+        </main>
+      </React.Fragment>
+
+/*      <form onSubmit={e => this.props.handle_login(e, this.state)}>
         <h4>Log In</h4>
         <label htmlFor="username">Username</label>
         <input
@@ -36,13 +130,14 @@ class LoginForm extends React.Component {
           onChange={this.handle_change}
         />
         <input type="submit" />
-      </form>
+      </form>*/
     );
   }
 }
 
-export default LoginForm;
+export default withStyles(styles)(LoginForm);
 
 LoginForm.propTypes = {
-  handle_login: PropTypes.func.isRequired
+  handle_login: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
 };
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,6 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import styles from '../styles/material-ui/styles'
 
+function Nav(props) {
+  const {component: Component, ...classes} = props.classes;
+  const logged_out_nav = (
+    <div>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit" className={classes.navlink} onClick={() => props.display_form('login')}>Login</Button>
+            <Button color="inherit" className={classes.navlink} onClick={() => props.display_form('signup')}>Sign Up</Button>
+              <div class="title">
+                  The Maths Quiz
+              </div>
+            <div class="user-info">
+                Please Log In
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </div>
+  );
+
+  const logged_in_nav = (
+    <div>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit" className={classes.navlink} onClick={props.handle_logout}>Logout</Button>
+              <div class="title">
+                  The Maths Quiz
+              </div>
+              <div class="user-info">
+                Hello {props.username}
+              </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </div>
+
+  )
+  return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
+}
+
+/*
 function Nav(props) {
   const logged_out_nav = (
     <ul>
@@ -16,11 +64,13 @@ function Nav(props) {
   );
   return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
 }
+*/
 
-export default Nav;
+export default withStyles(styles)(Nav)
 
 Nav.propTypes = {
   logged_in: PropTypes.bool.isRequired,
   display_form: PropTypes.func.isRequired,
-  handle_logout: PropTypes.func.isRequired
+  handle_logout: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired
 };
