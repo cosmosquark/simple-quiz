417e1509bb63d47ad2e99161ee429f1c66f4737e
Cosmosquark <cosmosquark@sector-alpha.net>
2019-01-18 00:14:44 +0000

    made signup form

--- front-end/src/components/SignupForm.js
+++ front-end/src/components/SignupForm.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import '../styles/sign-up.css'
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
 
 class SignupForm extends React.Component {
   state = {
     username: '',
+    first_name: '',
     password: ''
   };
 
@@ -18,14 +42,24 @@ class SignupForm extends React.Component {
   };
 
   render() {
+    const {component: Component, ...classes} = this.props.classes
     return (
-      <form onSubmit={e => this.props.handle_signup(e, this.state)}>
+      <div>
+      <div className="container">
+        <form onSubmit={e => this.props.handle_signup(e, this.state)}>
         <h4>Sign Up</h4>
         <label htmlFor="username">Username</label>
         <input
           type="text"
           name="username"
           value={this.state.username}
+          onChange={this.handle_change}
+        />
+      <label htmlFor="first_name">First Name</label>
+        <input
+          type="text"
+          name="first_name"
+          value={this.state.first_name}
           onChange={this.handle_change}
         />
         <label htmlFor="password">Password</label>
@@ -35,13 +69,15 @@ class SignupForm extends React.Component {
           value={this.state.password}
           onChange={this.handle_change}
         />
-        <input type="submit" />
+        <input class="signup-button" type="submit" value="Complete Sign Up"/>
       </form>
+    </div>
+  </div>
     );
   }
 }
 
-export default SignupForm;
+export default withStyles(styles)(SignupForm)
 
 SignupForm.propTypes = {
   handle_signup: PropTypes.func.isRequired
